2c8d7827f03fc150628cac2ebac3a97c994004f2
Aleksandr Saiyan <anverch@gmail.com>
2020-04-08 23:13:01 -0500

    add columns

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,21 @@ $("#currentDay").text(moment().format('dddd, MMMM Do'));
 // Table inside the container 
 var table = $("<table>");
 $(".container").append(table);
-table.addClass("time-block");
 
 // for loop for the 8 rows for 8 hours of work day 
 for (var i=0; i < 9; i++) {
     var tableRow = $("<tr>");
     var rowTime = moment().hours(i+9);
-    tableRow.text(rowTime.format('h A'));
     table.append(tableRow);
-    tableRow.addClass("row " + getRowClass(rowTime.hour()));  
+    tableRow.addClass("row");
+    var column1 = $("<td>"); 
+    var column2 = $("<td>"); 
+    var column3 = $("<td>"); 
+    tableRow.append(column1, column2, column3);
+    column1.addClass("hour").text(rowTime.format('h A'));
+    column2.addClass("description " + getRowClass(rowTime.hour()));
+    column3.addClass("time-block");
+
 }
 
 // function that compares current time to our table time 
