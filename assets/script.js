da5d5834c657faac915cafabf6f61cf9cb9b21ad
Aleksandr Saiyan <anverch@gmail.com>
2020-04-08 22:46:00 -0500

    add class to row

--- assets/script.js
+++ assets/script.js
@@ -9,9 +9,24 @@ table.addClass("time-block");
 // for loop for the 8 rows for 8 hours of work day 
 for (var i=0; i < 9; i++) {
     var tableRow = $("<tr>");
-    tableRow.text(moment().hours(i+9).format('h A'));
+    var rowTime = moment().hours(i+9);
+    tableRow.text(rowTime.format('h A'));
     table.append(tableRow);
-    tableRow.addClass("row");  
+    tableRow.addClass("row " + getRowClass(rowTime.hour()));  
 }
 
+// function that compares current time to our table time 
+function getRowClass (time){
+    var currentTime = moment().hour();
+    if (time < currentTime){
+        return  "past";
+        //past
+    } else if (time===currentTime) {
+        return "present";
+        //current
+    } else {
+        return "future";
+        //future
+    }
+}
 
