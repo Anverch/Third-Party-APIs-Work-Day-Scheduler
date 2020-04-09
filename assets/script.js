e153ad3df39e0cefaa6fb46aa3c5eb528e8e19db
Aleksandr Saiyan <anverch@gmail.com>
2020-04-08 21:51:32 -0500

    add table & table row

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,16 @@
+// Display current day
 $("#currentDay").text(moment().format('dddd, MMMM Do'));
+
+// Table inside the container 
+var table = $("<table>");
+$(".container").append(table);
+
+// for loop for the 8 rows for 8 hours of work day 
+for (var i=0; i < 8; i++) {
+    var tableRow = $("<tr>");
+    tableRow.text("Tr" + i);
+    table.append(tableRow);
+      
+}
+
+
