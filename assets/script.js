61b9dcffaf02dd36cdba9ae31609d952ebf52183
Aleksandr Saiyan <anverch@gmail.com>
2020-04-10 18:53:01 -0500

    fix columns

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,30 @@
 $("#currentDay").text(moment().format('dddd, MMMM Do'));
 
 // Table inside the container 
-var table = $("<table>");
+var table = $("<div>");
 $(".container").append(table);
 
 // for loop for the 8 rows for 8 hours of work day 
 for (var i=0; i < 9; i++) {
-    var tableRow = $("<tr>");
+    var tableRow = $("<div>");
     var rowTime = moment().hours(i+9);
+
     table.append(tableRow);
-    tableRow.addClass("row");
-    var column1 = $("<td>"); 
-    var column2 = $("<td>"); 
-    var column3 = $("<td>"); 
+    tableRow.addClass("row time-block");
+
+    var column1 = $("<div>");
+    column1.addClass("col-md-1");
+
+    var column2 = $("<textarea>"); 
+    column2.addClass("col-md-10");
+
+    var column3 = $("<div>"); 
+    column3.addClass("col-md-1");
+    
     tableRow.append(column1, column2, column3);
     column1.addClass("hour").text(rowTime.format('h A'));
     column2.addClass("description " + getRowClass(rowTime.hour()));
-    column3.addClass("time-block");
+    column3.addClass(" saveBtn");
 
 }
 
