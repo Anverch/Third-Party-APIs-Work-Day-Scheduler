3132c35882ee37926cbdaf601eb7904ea331da11
Aleksandr Saiyan <anverch@gmail.com>
2020-04-11 19:28:42 -0500

    add save icon

--- assets/script.js
+++ assets/script.js
@@ -5,22 +5,30 @@ $("#currentDay").text(moment().format('dddd, MMMM Do'));
 var table = $("<div>");
 $(".container").append(table);
 
+//Array to store added text in a row
+var dataStorage = [];
+
 // for loop for the 8 rows for 8 hours of work day 
 for (var i=0; i < 9; i++) {
     var tableRow = $("<div>");
     var rowTime = moment().hours(i+9);
 
     table.append(tableRow);
-    tableRow.addClass("row time-block");
+    tableRow.addClass("row time-block").attr('id', i);
 
     var workHour = $("<div>").addClass("col-md-1");
     var workEvent = $("<textarea>").addClass("col-md-10"); 
     var saveBtn = $("<button>").addClass("col-md-1"); 
     
-    tableRow.append(workHour, workEvent, saveBtn);
+    tableRow.append(workHour, workEvent, saveBtn);    
     workHour.addClass("hour").text(rowTime.format('h A'));
     workEvent.addClass("description " + getRowClass(rowTime.hour()));
-    saveBtn.addClass("saveBtn");
+    saveBtn.addClass("saveBtn fas fa-save");
+
+    saveBtn.click(function(){
+        dataStorage[i] = workEvent.val();
+        console.log(dataStorage)
+    });
 
 }
 
