55a46e21f73ab109446fc088db561545573cc6a8
Aleksandr Saiyan <anverch@gmail.com>
2020-04-11 23:29:09 -0500

    add comments

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,19 @@
 // Display current day
 $("#currentDay").text(moment().format('dddd, MMMM Do'));
 
-// Table inside the container 
+// Div inside the container 
 var table = $("<div>");
 $(".container").append(table);
 
-//Array to store added text in a row
-var dataStorage = JSON.parse(localStorage.getItem("savedText")) || [];
+// Array to store user input
+var storageKey = "savedText";
+var dataStorage = JSON.parse(localStorage.getItem(storageKey)) || [];
+var numberWorkHours = 8;
 
-// for loop for the 8 rows for 8 hours of work day 
-for (var i=0; i < 9; i++) {
+// For loop for the 8 rows for 8 hours of work day 
+for (var i = 0; i <= numberWorkHours; i++) {
     var tableRow = $("<div>");
-    var rowTime = moment().hours(i+9);
+    var rowTime = moment().hours(i + 9);
 
     table.append(tableRow);
     tableRow.addClass("row time-block").attr('row-id', i);
@@ -20,38 +22,40 @@ for (var i=0; i < 9; i++) {
         .addClass("col-md-1 hour")
         .text(rowTime.format('h A'));
 
-    var workEvent = $("<textarea>")
+    var timeBlockText = $("<textarea>")
         .addClass("col-md-10 description " + getRowClass(rowTime.hour()))
-        .attr('id', "event-"+i)
-        .text(dataStorage[i]); 
+        .attr('id', "event-" + i)
+        .text(dataStorage[i]);
 
     var saveBtn = $("<button>")
         .addClass("col-md-1 saveBtn fas fa-save")
-        .attr('id', "button-"+i); 
-    
-    tableRow.append(workHour, workEvent, saveBtn);    
+        .attr('id', i);
 
-    saveBtn.click(function(e){
-        dataStorage[e.target.id.replace("button-", "")] = workEvent.text();
-        console.log(dataStorage);
-        localStorage.setItem("savedText", JSON.stringify(dataStorage));
+    tableRow.append(workHour, timeBlockText, saveBtn);
+
+    saveBtn.click(function (event) {
+        var buttonIndex = event.target.id;
+        dataStorage[buttonIndex] = timeBlockText.text();
+        localStorage.setItem(storageKey, JSON.stringify(dataStorage));
     });
-    workEvent.keyup(function(e){
-        workEvent.text(e.target.value);
+
+    // Event listener for user input, assigns value to work event 
+    timeBlockText.keyup(function (event) {
+        timeBlockText.text(event.target.value);
     });
 }
 
-// function that compares current time to our table time 
-function getRowClass (time){
+/**
+ * Function that returns time block class name based on relative time
+ * @param {Object} time The time of the time block 
+ */
+function getRowClass(time) {
     var currentTime = moment().hour();
-    if (time < currentTime){
-        return  "past";
-        //past
-    } else if (time===currentTime) {
+    if (time < currentTime) {
+        return "past";
+    } else if (time === currentTime) {
         return "present";
-        //current
     } else {
         return "future";
-        //future
     }
 }
